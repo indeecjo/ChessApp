2412b57d8370b6d4bf42ab8c843edd1117c4a45b
Moti Korets <indeecjo@gmail.com>
2014-05-18 13:30:57 +0300

    Revert "Change file structure"
    This reverts commit 75e4c95f03804197fdbe437fddae69ae50ad6be6.

--- board/knight.js
+++ board/knight.js
@@ -1,2 +1,28 @@
 'use strict';
 
+chessApp.factory('Knight', function(Piece){
+  function Knight(coord,color){
+    Piece.apply(this,arguments);      
+  }
+
+  Knight.prototype = Object.create(Piece.prototype);
+
+  Knight.prototype.getUnicodeChar = function(){
+    if(this.color == Piece.WHITE){
+      return '\u2658';
+    }else if(this.color == Piece.BLACK){
+      return '\u265E';
+    }        
+  }
+
+  Knight.prototype.isLegalMove = function(newCoord){
+    if((Math.abs(this.x-newCoord.x ) == 1 && Math.abs(this.y-newCoord.y) == 2 )|| (Math.abs(this.x-newCoord.x ) == 2 && Math.abs(this.y-newCoord.y) == 1 )){
+      return true;
+    }else{
+      return false;
+    }
+  }
+
+
+ return Knight;
+});
